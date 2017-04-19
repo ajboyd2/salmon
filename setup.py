99c8e4e91d257f9c2c76ceff7b7607237bcb4f77
ajboyd2 <ajboyd@calpoly.edu>
2017-04-19 13:18:42 -0700

    Initial setup of directories and files.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="symbolic-modeling",
+    version="0.2.0",
+
+    description="A symbolic statistical modeling tool.",
+
+    url="https://github.com/ajboyd2/statistical-modeling",
+
+    author="Alex Boyd",
+    author_email="ajboyd@calpoly.edu",
+
+    license="GPLv3",
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+    ],
+    
+    keywords='modeling symbolic regression',
+
+    packages=find_packages()
+)
