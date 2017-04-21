0a22349589e5c966c76fb62d6a91fb121a472e88
ajboyd2 <ajboyd@calpoly.edu>
2017-04-21 10:00:31 -0700

    Simple linear regression works for fitting and predicting on a model.

--- symbolic_modeling/model.py
+++ symbolic_modeling/model.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 
-from .expression import Expression, Mono, Interaction, Combination
-
 class Model:
     def __init__(self):
         raise NotImplementedError()
@@ -19,24 +17,33 @@ class LinearModel(Model):
         self.ex = explanatory
         self.re = response
         self.bhat = None
+        self.training_data = None
 
     def fit(self, data):
         # Construct X matrix
-        X = extract_columns(self.ex, data)
+        X = LinearModel.extract_columns(self.ex, data)
+        X = pd.concat([LinearModel.ones_column(data), X], axis = 1)
+        self.training_data = X
         # Construct Y vector
-        y = extract_columns(self.re, data)
+        y = LinearModel.extract_columns(self.re, data)
         # Ensure correct dimensionality of y
         if len(y.shape) > 1 and y.shape[1] > 1:
             raise Exception("Response variable of linear model can only be a single term expression.")
         # Solve equation
-        self.bhat = np.linalg.solve(X.T @ X, X.T @ y)
+        self.bhat = pd.DataFrame(np.linalg.solve(np.dot(X.T, X), np.dot(X.T, y)), 
+                                 index=X.columns, columns = ["Weights"])
         return self.bhat
         
     def predict(self, data):
         # Construct the X matrix
-        X = extract_columns(self.ex, data)
+        X = LinearModel.extract_columns(self.ex, data)
+        X = pd.concat([LinearModel.ones_column(data), X], axis = 1)
         # Multiply the weights to each column and sum across rows
-        return (X @ self.bhat).sum(axis = 1)
+        return pd.DataFrame({"Results" : np.dot(X, self.bhat).sum(axis = 1)})
+        
+    # static method
+    def ones_column(data):
+        return pd.DataFrame({"Bias" : np.repeat(1, data.shape[0])})
         
     # static method
     def extract_columns(expr, data):
@@ -50,12 +57,12 @@ class LinearModel(Model):
         elif isinstance(expr, Interaction):
             columns = [extract_columns(e, data) for e in expr.flatten(True)]
             product = pd.concat(columns, axis = 1).prod(axis = 0)
-            return transform(expr, product)
+            return LinearModel.transform(expr, product)
         
         elif isinstance(expr, Mono):
             if expr.name not in list(data):
                 raise Exception("Variable { " + expr.name + " } not found within data.")
-            return transform(expr, data[expr.name])
+            return LinearModel.transform(expr, pd.DataFrame(data[expr.name]))
             
         else:
             raise Exception("LinearModel only suppoprts expressions consisting of Mono, Interaction, and Combination.")
