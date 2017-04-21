6875351e4d2a74eaaf02615e6a9b7a773c944fd9
ajboyd2 <ajboyd@calpoly.edu>
2017-04-20 22:39:07 -0700

    Initial commit of fitting for the linear model.

--- symbolic_modeling/model.py
+++ symbolic_modeling/model.py
@@ -22,26 +22,49 @@ class LinearModel(Model):
 
     def fit(self, data):
         # Construct X matrix
-        X = None
+        X = extract_columns(self.ex, data)
         # Construct Y vector
-        y = None
+        y = extract_columns(self.re, data)
+        # Ensure correct dimensionality of y
+        if len(y.shape) > 1 and y.shape[1] > 1:
+            raise Exception("Response variable of linear model can only be a single term expression.")
         # Solve equation
         self.bhat = np.linalg.solve(X.T @ X, X.T @ y)
         return self.bhat
         
     def predict(self, data):
         # Construct the X matrix
-        X = None
+        X = extract_columns(self.ex, data)
         # Multiply the weights to each column and sum across rows
         return (X @ self.bhat).sum(axis = 1)
         
     # static method
     def extract_columns(expr, data):
+        if not isinstance(data, (pd.DataFrame, pd.Series)):
+            raise Exception("Only DataFrames and Series are supported for LinearModel.")
+    
         if isinstance(expr, Combination):
-            pass
+            columns = [extract_columns(e, data) for e in expr.flatten()]
+            return pd.concat(columns, axis = 1)
+            
         elif isinstance(expr, Interaction):
-            pass
+            columns = [extract_columns(e, data) for e in expr.flatten(True)]
+            product = pd.concat(columns, axis = 1).prod(axis = 0)
+            return transform(expr, product)
+        
         elif isinstance(expr, Mono):
-            pass
+            if expr.name not in list(data):
+                raise Exception("Variable { " + expr.name + " } not found within data.")
+            return transform(expr, data[expr.name])
+            
         else:
             raise Exception("LinearModel only suppoprts expressions consisting of Mono, Interaction, and Combination.")
+            
+    # static method
+    def transform(expr, data):
+        if not isinstance(expr, Mono):
+            raise Exception("Transformation of data is only supported on singular terms of expressions.")
+        
+        return ((data + expr.shift) * expr.coefficient) ** expr.transformation
+
+        
