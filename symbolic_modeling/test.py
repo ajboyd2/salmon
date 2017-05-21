ec920b487bbd39a3129d114de31ccb4d1602cf7e
ajboyd2 <ajboyd@calpoly.edu>
2017-05-21 11:58:05 -0700

    Added test file.

---
+++ symbolic_modeling/test.py
@@ -0,0 +1,162 @@
+import unittest
+from Expression import *
+from Model import *
+
+# Expressions
+class TestVarMethods(unittest.TestCase):
+    
+    def test_(self):
+        self.assertEqual()
+        
+    def test_(self):
+        self.assertEqual()
+        
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+
+class TestQuantitativeMethods(unittest.TestCase):
+    
+    def test_(self):
+        self.assertEqual()
+        
+    def test_(self):
+        self.assertEqual()
+        
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+
+class TestCategoricalMethods(unittest.TestCase):
+    
+    def test_(self):
+        self.assertEqual()
+        
+    def test_(self):
+        self.assertEqual()
+        
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+
+class TestInteractionMethods(unittest.TestCase):
+    
+    def test_(self):
+        self.assertEqual()
+        
+    def test_(self):
+        self.assertEqual()
+        
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+
+class TestCombinationMethods(unittest.TestCase):
+    
+    def test_(self):
+        self.assertEqual()
+        
+    def test_(self):
+        self.assertEqual()
+        
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+
+        
+class TestModelMethods(unittest.TestCase):
+    
+    def test_(self):
+        self.assertEqual()
+        
+    def test_(self):
+        self.assertEqual()
+        
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+                
+    def test_(self):
+        self.assertEqual()
+        
