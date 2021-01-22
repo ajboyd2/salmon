3cb068eedd41bc8530a158aa819a952ec82fff3c
Dennis Sun <dsun09@calpoly.edu>
2021-01-21 18:03:23 -0800

    upgrading version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="salmon-lm",
-    version="1.0.1",
+    version="1.0.2",
 
     description="A symbolic algebra based linear regression tool.",
 
