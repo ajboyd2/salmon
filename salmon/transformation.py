a0021ce01a37f83a88943e32c109abbd947b1730
Unknown <ajboyd@calpoly.edu>
2021-02-25 23:30:36 -0800

    Transformation.py now has consistent style.

--- salmon/transformation.py
+++ salmon/transformation.py
@@ -1,3 +1,5 @@
+"""Contains object representation of possible variable transformations."""
+
 import pandas as pd
 import numpy as np
 
@@ -6,102 +8,107 @@ import numpy as np
 # with how to display / print the transformation
 
 class Transformation():
-    ''' A Trasformation object holds the actual function to calculate transformations 
-    on data as well as some helper information for printing and visualizing. 
-    '''
+    """A Trasformation object holds the actual function to calculate
+    transformations on data as well as some helper information for
+    printing and visualizing.
+    """
     
-    def __init__(self, func, pattern, name, inverse = None):
-        ''' Creates a Transformation object.
+    def __init__(self, func, pattern, name, inverse=None):
+        """Creates a Transformation object.
 
         Arguments:
             func - A function to be applied to a column of data in a DataFrame.
             pattern - A str holding a template for printing.
             name - A str describing the transformation.
             inverse - An optional function that will undo the func operation.
-        '''
+        """
         self.func = func
         self.pattern = pattern
         self.inverse = inverse
         self.name = name
         
     def __str__(self):
-        ''' Returns the given pattern for debugging. '''
+        """Returns the given pattern for debugging. """
         return self.pattern
 
     def __eq__(self, other):
-        ''' Check if two objects are equivalent. 
+        """Check if two objects are equivalent. 
 
         Arguments:
             other - An object.
 
         Returns:
-            A boolean, True if other is a Transformation and equivalent, False if else.
-        '''
+            A boolean, True if other is a Transformation and equivalent, False
+            if else.
+        """
         if isinstance(other, Transformation):
             return self.pattern == other.pattern and self.name == other.name
         else:
             return False
 
     def __hash__(self):
-        ''' Hash the transformation for the purposes of storing in sets and dictionaries.
+        """Hash the transformation for the purposes of storing in sets and
+        dictionaries.
 
         Returns:
             A real value representing the hash of the Transformation.
-        '''
+        """
 
         return hash((self.pattern, self.name))
         
     def compose(self, inner):
-        ''' Applies specific data to the pattern for printing. 
+        """Applies specific data to the pattern for printing. 
         
         Arguments:
             inner - An object to be injested by str.format()
         
         Returns:
             A processed and evaluated str representing the Trasnformation.
-        '''
+        """
         return self.pattern.format(inner)
         
-    def transform(self, values, training = True):
-        ''' Apply the function to the data.
+    def transform(self, values, training=True):
+        """Apply the function to the data.
 
         Arguments:
             values - A Series object that is the data to trasnform.
-            training - A flag to indicate is this transformation is during training or not. Default is True.
+            training - A flag to indicate is this transformation is during
+                training or not. Default is True.
 
         Returns:
             A trasnformed Series. 
-        '''
+        """
         return self.func(values)
     
     def copy(self):
-        ''' Returns a deep copy of the Transformation. '''
+        """Returns a deep copy of the Transformation."""
         return Transformation(self.func, self.pattern, self.name, self.inverse)
 
     def invert(self, data):
-        ''' If available, invert the data.
+        """If available, invert the data.
 
         Arguments:
             data - A Series object holding the data to invert.
 
         Returns:
             A Series object that has the inverted data.
-        '''
+        """
         if self.inverse is None:
             raise Exception("Inverse not defined for " + self.name + " transformation.")
 
         return self.inverse(data)
 
 class Center(Transformation):
-    ''' A specific type of Trasnformation for centering data so that it has a mean of 0. '''
+    """A specific type of Trasnformation for centering data so that it has a
+    mean of 0."""
 
     def __init__(self):
-        ''' Create a Center object. '''
+        """Create a Center object."""
         self.pattern = "{0}-E({0})"
         self.past_mean = 0
         self.name = "Center"
         
-    def transform(self, values, training = True):
+    def transform(self, values, training=True):
         if training:
             self.past_mean = values.mean()
         
@@ -116,9 +123,11 @@ class Center(Transformation):
         return data + self.past_mean
 
 class Standardize(Transformation):
-    ''' A specific type of Transformation that standardizes the data so that it has a mean of 0 and standard deviation of 1. '''
+    """A specific type of Transformation that standardizes the data so that it
+    has a mean of 0 and standard deviation of 1."""
+    
     def __init__(self):
-        ''' Create a Standardize object. '''
+        """Create a Standardize object. """
         self.pattern = "({0}-E({0}))/Std({0})"
         self.past_mean = 0
         self.past_std = 1
